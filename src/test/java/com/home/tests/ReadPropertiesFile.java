b4bd71d29e1a316fccbc5a4e9ca76ad0b5d8ed22
gitskota <subbarao.kota@gmail.com>
2019-05-12 20:44:48 -0400

    Moved Apllication.java to src/main

--- src/test/java/com/home/tests/ReadPropertiesFile.java
+++ src/test/java/com/home/tests/ReadPropertiesFile.java
@@ -1,29 +1,26 @@
 package com.home.tests;
 
 import com.home.props.Application;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.core.env.Environment;
 import org.testng.annotations.Test;
 
-@SpringBootTest(classes = Application.class)
-@EnableConfigurationProperties(ConfigProperties.class)
+@SpringBootTest//(classes = Application.class)
+@SpringBootConfiguration
+//@EnableConfigurationProperties(ConfigProperties.class)
 
 public class ReadPropertiesFile extends ConfigProperties {
 
-    ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ConfigProperties.class);
-    ConfigProperties configProperties = applicationContext.getBean(ConfigProperties.class);
-
-    String url = configProperties.url;
-    String serverPort = configProperties.serverPort;
+//    ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ConfigProperties.class);
+//    ConfigProperties configProperties = applicationContext.getBean(ConfigProperties.class);
 
     @Test
     public void getPropertiesFromPropertiesFile(){
 
-        System.out.println("\napp.uri from application.properties file: "+ url);
+        System.out.println("app.uri from application.properties file: "+ url);
         System.out.println("\nserver.port from application.properties file: "+ serverPort);
     }
 
